54c2effd3c203c123a89e2ee71a2bc318a6fdf1e
apeus <1034313175@qq.com>
2022-05-29 22:31:26 +0800

    add index page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , render_template
 from flask import url_for
 app = Flask(__name__)
 
@@ -7,8 +7,9 @@ def user_page(name):
     return 'USER: %s' %name
 
 @app.route('/')
-def hello():
-    return 'hello'
+def index():
+    return 	render_template('index.html',	name=name,	movies=movies)
+
 
 @app.route('/test')
 def test_url_for():
@@ -21,3 +22,16 @@ def test_url_for():
     print(url_for('test_url_for', num=42))
 
     return 'test page'
+
+name = 'apeus'
+movies = [	{'title':	'My	Neighbor	Totoro',	'year':	'1988'},	
+			{'title':	'Dead	Poets	Society',	'year':	'1989'},	
+        	{'title':	'A	Perfect	World',	'year':	'1993'},			
+            {'title':	'Leon',	'year':	'1994'},		
+        	{'title':	'Mahjong',	'year':	'1996'},	
+        	{'title':	'Swallowtail	Butterfly',	'year':	'1996'},
+         	{'title':	'King	of	Comedy',	'year':	'1999'},		
+        	{'title':	'Devils	on	the	Doorstep',	'year':	'1999'},		
+        	{'title':	'WALL-E',	'year':	'2008'},
+            {'title':	'The	Pork	of	Music',	'year':	'2012'},
+        ]
